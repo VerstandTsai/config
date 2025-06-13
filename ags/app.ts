444dbe5b8ae906fea527a436f062fef8033f1505
Verstand Tsai <hackertsaigenz@gmail.com>
2025-06-13 11:04:25 +0800

    Using AGS to implement the powermenu, replacing wofi

--- ags/app.ts
+++ ags/app.ts
@@ -2,11 +2,13 @@ import { App } from "astal/gtk3"
 import style from "./style.scss"
 import Bar from "./widget/Bar"
 import Wallpaper from "./widget/Wallpaper"
+import PowerMenu from "./widget/PowerMenu"
 
 App.start({
   css: style,
   main() {
     App.get_monitors().map(Bar)
     App.get_monitors().map(Wallpaper)
+    App.get_monitors().map(PowerMenu)
   },
 })
