60ea6306a7a62bbff75f0df607e9decbcd4fe2bd
Verstand Tsai <hackertsaigenz@gmail.com>
2025-07-07 10:03:21 +0800

    Some changes

--- hypr/shot.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-filename="$HOME/Pictures/screenshots/$(date '+%F-%H-%M-%S').png"
-
-grim -g "$(slurp)" "$filename"
-notify-send -i "$filename" 'Screenshot taken' "Picture saved to $filename"
-
