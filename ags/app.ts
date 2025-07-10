130d9152031bce4ce5e0b40bdbe7d571fa953518
Verstand Tsai <hackertsaigenz@gmail.com>
2025-07-10 16:23:41 +0800

    Created a dock in AGS

--- ags/app.ts
+++ ags/app.ts
@@ -2,11 +2,13 @@ import app from "ags/gtk4/app"
 import style from "./style.scss"
 import Bar from "./widget/Bar"
 import Launcher from "./widget/Launcher"
+import Dock from "./widget/Dock"
 
 app.start({
   css: style,
   main() {
     app.get_monitors().map(Bar)
     app.get_monitors().map(Launcher)
+    app.get_monitors().map(Dock)
   },
 })
---
+++ ags/widget/Dock.tsx
@@ -0,0 +1,42 @@
+import app from "ags/gtk4/app"
+import { Astal, Gtk, Gdk } from "ags/gtk4"
+import Apps from "gi://AstalApps"
+
+export default function Dock(gdkmonitor: Gdk.Monitor) {
+  const apps = new Apps.Apps()
+  const pinnedApps = [
+    "firefox",
+    "thunderbird",
+    "discord",
+    "kitty",
+    "vlc",
+    "obs",
+    "steam"
+  ].map((name) => apps.fuzzy_query(name)[0])
+
+  return (
+    <window
+      visible
+      name="dock"
+      class="Dock"
+      gdkmonitor={gdkmonitor}
+      exclusivity={Astal.Exclusivity.IGNORE}
+      anchor={Astal.WindowAnchor.BOTTOM}
+      application={app}
+      $={(self) => self.marginBottom = -128}
+    >
+      <Gtk.EventControllerMotion
+        onEnter={({widget}) => widget.marginBottom = 0}
+        onLeave={({widget}) => widget.marginBottom = -128}
+      />
+      <box>
+        {pinnedApps.map((pinned) =>
+          <button class="AppButton" onClicked={() => pinned.launch()}>
+            <image iconName={pinned.iconName} pixelSize={64}/>
+          </button>
+        )}
+      </box>
+    </window>
+  )
+}
+
