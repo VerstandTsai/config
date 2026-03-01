74973ba989d79889d8497c51434acf078c677fb6
Verstand Tsai <hackertsaigenz@gmail.com>
2026-03-01 15:13:21 +0800

    Embed custom css in revealjs output

--- nvim/ftplugin/markdown.lua
+++ nvim/ftplugin/markdown.lua
@@ -1,2 +1,2 @@
 vim.keymap.set('n', '<a-c>', ':silent !pandoc -f markdown+east_asian_line_breaks % -o %:r.html -s --mathjax -c ~/Templates/css/markdown.css<cr>')
-vim.keymap.set('n', '<a-C>', ':silent !pandoc -f markdown+east_asian_line_breaks % -o %:r.html -s --mathjax -c ~/Templates/css/revealjs.css -t revealjs<cr>')
+vim.keymap.set('n', '<a-C>', ':silent !pandoc -f markdown+east_asian_line_breaks % -o %:r.html -s --mathjax -H ~/Templates/css/revealjs.html -t revealjs<cr>')
