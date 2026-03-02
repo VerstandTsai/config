d2dfae422e35b526549cb659979c7873f0b40d6a
Verstand Tsai <hackertsaigenz@gmail.com>
2026-03-02 11:12:14 +0800

    Added incremental list

--- nvim/ftplugin/markdown.lua
+++ nvim/ftplugin/markdown.lua
@@ -1,2 +1,2 @@
 vim.keymap.set('n', '<a-c>', ':silent !pandoc -f markdown+east_asian_line_breaks % -o %:r.html -s --mathjax -c ~/Templates/css/markdown.css<cr>')
-vim.keymap.set('n', '<a-C>', ':silent !pandoc -f markdown+east_asian_line_breaks % -o %:r.html -s --mathjax -H ~/Templates/css/revealjs.html -t revealjs --highlight-style=breezedark<cr>')
+vim.keymap.set('n', '<a-C>', ':silent !pandoc -f markdown+east_asian_line_breaks % -o %:r.html -s --mathjax -H ~/Templates/css/revealjs.html -t revealjs -i --highlight-style=breezedark<cr>')
