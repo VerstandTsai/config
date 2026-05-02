6dcc93e24a409e35be37063efc7bdd06c250970d
Verstand Tsai <hackertsaigenz@gmail.com>
2026-05-03 07:44:31 +0800

    Removed Markdown ftplugin

--- nvim/ftplugin/markdown.lua
+++
@@ -1,1 +0,0 @@
-vim.keymap.set('n', '<a-c>', ':silent !pandoc -f markdown+east_asian_line_breaks % -o %:r.html -s --mathjax -H ~/Templates/css/revealjs.html -t revealjs -i --highlight-style=breezedark<cr>')
